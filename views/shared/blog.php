31c420a69f964b25f93a97919fb0d5cabb370b36
J <jcmundy@emory.edu>
2021-01-26 11:19:55 -0500

    custom formatting of posts plus update to php 7.2

--- views/shared/blog.php
+++ views/shared/blog.php
@@ -13,17 +13,28 @@
                 <?php if($links): ?>
                     <span class="title">
                     <a href="<?php echo $post->link; ?>">
-                        <?php echo $post->title();?>
+                    <?php if(is_array($post->title())): ?>
+                      <?php $green = $post->title(); $blue = array_slice($green, -1, 1); $purple = print_r($blue, true); $pink = substr($purple, strpos($purple, "[textContent] => ")); $yellow = substr($pink, 16, -6);?>
+                          <span style="font-size:130%"><?php echo substr($yellow, 0, 65);?><?php echo "..."?></span><br/>
+                          <span style="font-size:80%"><?php echo $post->date();?></span>
                     </a>
-
-                    <div class="body">
-                        <?php echo getFeedItemBody($post, $display); ?>
+                    <?php else: ?>
+                    <a href="<?php echo $post->link; ?>">
+                      <!-- <span style="font-size:130%"><?php /*echo $post->title();*/?></span><br/> -->
+                        <span style="font-size:130%"><?php echo substr($post->title(), 0, 65);?><?php echo "..."?></span><br/>
+                        <span style="font-size:80%"><?php echo substr($post->pubDate(), 0, 17);?></span>
+                    </a>
+                    <?php endif; ?>
+                    <button class="accordion">Description</button>
+                    <div class="panel">
+                        <?php echo getFeedItemBody($post, $display); ?><br/>
                     </div>
                     </span>
                 <?php else: ?>
                     <div class="title"><?php echo $post->title();?></div>
-                    <div class="body">
-                        <?php echo getFeedItemBody($post, $display); ?>
+                    <button class="accordion">Description</button>
+                    <div class="panel">
+                        <?php echo getFeedItemBody($post, $display); ?><br/>
                     </div>
                 <?php endif; ?>
             </li>
